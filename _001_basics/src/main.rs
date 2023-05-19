f23e95c76c1b31bf9542ab701576ad1760ac50f5
AfroGrit <jlkitunka@gmail.com>
2023-05-19 21:30:58 +0200

    chore: variables in rust, check the scope

---
+++ _001_basics/src/main.rs
@@ -0,0 +1,13 @@
+fn main() {
+
+    // variables in rust
+    let mut x = 4;
+    println!("Hello, x is {}", x);
+    {
+        let y = 41;
+        println!("Hello, y is {}", y);
+    }
+
+    
+
+}
