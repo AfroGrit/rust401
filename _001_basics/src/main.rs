8b23dccc1c7c8500043706a12bc87b9619f02aca
AfroGrit <jlkitunka@gmail.com>
2023-05-19 21:47:59 +0200

    chore: data types

--- _001_basics/src/main.rs
+++ _001_basics/src/main.rs
@@ -1,13 +1,16 @@
 fn main() {
 
     // variables in rust
-    let mut x = 4;
+    let x = 4;
     println!("Hello, x is {}", x);
     {
         let y = 41;
         println!("Hello, y is {}", y);
     }
 
-    
+    let z: u32 = 23423;
+    println!("z is {}", z);
 
+    let tup: (i32, bool, char) = (1212, false, 'g');
+    println!("tupples, tup is {}", tup.0);
 }
